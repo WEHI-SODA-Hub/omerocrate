639298c3db6bbfe07a807778de63745faa780334
mcmero <7799828+mcmero@users.noreply.github.com>
2025-09-17 12:39:45 +1000

    Expose SegmentationUploader to CLI

--- src/omerocrate/__init__.py
+++ src/omerocrate/__init__.py
@@ -1,5 +1,5 @@
-from omerocrate.uploader import ApiUploader
+from omerocrate.uploader import ApiUploader, SegmentationUploader
 from omerocrate.taskqueue.upload import TaskqueueUploader
 from omerocrate.gateway import from_env
 
-__all__ = ["ApiUploader", "TaskqueueUploader", "from_env"]
+__all__ = ["ApiUploader", "SegmentationUploader", "TaskqueueUploader", "from_env"]
