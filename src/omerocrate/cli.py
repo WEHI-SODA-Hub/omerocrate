6dd090c84a20cfc9657d4e931a4b48d1867f7bb1
mcmero <7799828+mcmero@users.noreply.github.com>
2025-09-30 16:10:32 +1000

    Put Path | None in brackets

--- src/omerocrate/cli.py
+++ src/omerocrate/cli.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from pathlib import Path
-from typing import Annotated, Union
+from typing import Annotated
 import typer
 from importlib import import_module
 from asyncio import run
@@ -9,12 +9,13 @@ from omerocrate.uploader import OmeroUploader, SegmentationUploader
 
 app = typer.Typer(help="CLI for uploading RO-Crates to OMERO")
 
+
 @app.command(help="Upload an RO-Crate to OMERO")
 def upload(
     crate: Annotated[Path, typer.Argument(help="Path to the directory containing the RO-Crate")],
     uploader_path: Annotated[str, typer.Option("--uploader-path", "-u", help="Module path to the OmeroUploader class")] = "omerocrate.ApiUploader",
     seg_uploader_path: Annotated[str, typer.Option("--seg-uploader-path", "-s", help="Module path to the SegmentationUploader class")] = "omerocrate.SegmentationUploader",
-    seg_upload_dir: Annotated[Union[Path, None], typer.Option("--seg-upload-dir", "-d", help="Directory to upload segmentations")] = None
+    seg_upload_dir: Annotated[(Path | None), typer.Option("--seg-upload-dir", "-d", help="Directory to upload segmentations")] = None
 ):
     module_path, cls_name = uploader_path.rsplit('.', 1)
     module = import_module(module_path)
